7f00b8b74976a205629e42ead8a5810aeec4fc90
jytprks <36029299+jytprks@users.noreply.github.com>
2022-01-15 11:12:12 +0530

    Add hero section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,23 +4,68 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <div>
-      <div className={`container  ${styles.wrapper}`}>
+      <div className={`${styles.wrapper}`}>
         <div className={styles.heroName}>
           <div className={styles.helloWrap}>
-            <hr className={styles.helloHR}/>
+            <hr className={styles.helloHR} />
             <span className={styles.hello}>Hello</span>
           </div>
           <h1 className={styles.name}>I'M JYOTIPRAKASH GHORAI</h1>
-          <span>A FULL STACK WEB DEVELOPER</span>
+          <span className={styles.heroSubTitle}>
+            A FULL STACK WEB DEVELOPER
+          </span>
           <div className={styles.buttonWrap}>
-            <button className={styles.hireMe}>Hire Me</button>
+            <button className={styles.hireMeBtn}>Hire Me</button>
             <div>
-              <a href="#">Download CV</a>
-              <i class="fad fa-download"></i>
+            <div className={styles.downloadWrapper}>
+              <a className={styles.downloadLink} href="#">
+                Download CV
+              </a>
+              <i className={`fas fa-download ${styles.downloadPng}` }></i> 
             </div>
+            <hr className={styles.downloadBTNUnderLine}/>
+            </div>
+            
           </div>
         </div>
-        <div className={styles.heroImg}></div>
+        <div className={styles.heroImg}>
+          <img className={styles.img} src="/images/jyoti.jpg"/>
+        </div>
+        <div className={styles.socialLink}>
+        <i className={`fab fa-instagram fa-lg ${styles.socialItems}`}></i>
+        <i className={`fab fa-facebook-f fa-lg ${styles.socialItems}`}></i>
+        <i className={`fab fa-github fa-lg ${styles.socialItems}`}></i>
+        <i className={`fab fa-linkedin-in fa-lg ${styles.socialItems}`}></i>
+        <i className={`fab fa-twitter fa-lg ${styles.socialItems}`}></i>
+        <span className={styles.socialFollowMe}>Follow-</span>
+        </div>
+        <div className={styles.circles}>
+        <div className={styles.animatedWrap}>
+        <span ></span>
+        <span ></span>
+        <span ></span>
+        </div>
+
+        <div className={styles.animatedWrap1}>
+        <span ></span>
+        <span ></span>
+        <span ></span>
+        </div>
+
+
+        <div className={styles.animatedWrap2}>
+        <span ></span>
+        <span ></span>
+        <span ></span>
+        </div>
+
+        <div className={styles.animatedWrap3}>
+        <span ></span>
+        <span ></span>
+        <span ></span>
+        </div>
+        </div>
+        
       </div>
     </div>
   );
